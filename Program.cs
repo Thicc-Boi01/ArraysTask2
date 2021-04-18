7e4ba9cc5858edb799431df2f21a52a47260cc59
Thomas-Casey <102041956@student.swin.edu.au>
2021-04-19 03:03:35 +1000

    first

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArraysTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0 };
+            int sum = 0;
+            Array.ForEach(numbers, delegate (int i) { sum += i; });
+            Console.WriteLine("The sum of the Array is: " + sum);
+        }
+    }
+}
